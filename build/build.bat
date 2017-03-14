a435c497ca86388deddd9a57261358fcec6ce611
Niklas Lundberg <info@nlundberg.se>
2017-03-14 08:13:06 +0100

    Enabled MSBuild NuGet restore.

--- build/build.bat
+++ build/build.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
 SET Arbor.X.Tools.External.MSpec.Enabled=true
+SET Arbor.X.MSBuild.NuGetRestore.Enabled=true
 
 CALL "%~dp0\Build.exe"
 
